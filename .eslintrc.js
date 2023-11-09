13cf414fc1e6d4a6867038866f7ee90597a64059
choewy <choewy@trineedle.com>
2023-11-09 22:27:23 +0900

    feature/init

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  rules: {
+    "react-native/no-inline-styles": "off",
+    "react-hooks/rules-of-hooks": "off",
+    "react/no-unstable-nested-components": "off",
+    "react/react-in-jsx-scope": "off",
+    "linebreak-style": ["error", require("os").EOL === "\r\n" ? "windows" : "unix"],
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+    "max-len": [
+      "error",
+      {
+        code: 120,
+        tabWidth: 2,
+        ignoreComments: true,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  singleQuote: true,
+  semi: true,
+  ignoreComments: true,
+  ignoreTrailingComments: true,
+  ignoreUrls: true,
+  ignoreStrings: true,
+  ignoreTemplateLiterals: true,
+  ignoreRegExpLiterals: true,
+  trailingComma: "all",
+};

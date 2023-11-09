0088b7e7bea5a9bc382ae4eb410096b310b2a837
choewy <choewy@trineedle.com>
2023-11-10 01:20:17 +0900

    feature/styles

---
+++ src/common/styles/sign.style.ts
@@ -0,0 +1,10 @@
+import { SxProps } from '@mui/material';
+
+export class SignStyle {
+  public static Wrapper: SxProps = {
+    marginTop: 0,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  };
+}

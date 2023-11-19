08f1a80fff97886d2c1a93464247ac930d12a0b2
choewy <choewy32@gmail.com>
2023-11-19 14:15:15 +0900

    feature/recoil store hash key

--- src/core/store.ts
+++ src/core/store.ts
@@ -1,10 +1,11 @@
+import { v4 } from 'uuid';
 import { RecoilState, atom, useRecoilState, useRecoilValue, useResetRecoilState, useSetRecoilState } from 'recoil';
 
 export class RecoilStore<T> {
   private readonly store: RecoilState<T>;
 
   constructor(key: string, value?: T) {
-    this.store = atom({ key, default: value });
+    this.store = atom({ key: [key, v4()].join('-'), default: value });
   }
 
   useValue() {

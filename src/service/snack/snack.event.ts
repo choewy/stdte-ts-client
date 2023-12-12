7ca14c2a4cd700a1ac82ce58349e3e94bdc10295
choewy <choewy32@gmail.com>
2023-12-12 16:15:09 +0900

    feature/snack event

--- src/service/snack/snack.event.ts
+++ src/service/snack/snack.event.ts
@@ -1,18 +1,49 @@
 import { v4 } from 'uuid';
-import { VariantType } from 'notistack';
+
+import { HttpClientException } from '@core';
 
 import { SnackEventDetail } from './types';
 
 export class SnackEvent extends CustomEvent<SnackEventDetail> {
   static EVENT_NAME = 'snack_push';
 
-  constructor(variant: VariantType, message: string) {
-    super(SnackEvent.EVENT_NAME, {
-      detail: {
-        id: v4(),
-        variant,
-        message,
-      },
-    });
+  static dispatchByInfo(message: string) {
+    window.dispatchEvent(
+      new SnackEvent(this.EVENT_NAME, {
+        detail: { id: v4(), variant: 'info', message },
+      }),
+    );
+  }
+
+  static dispatchBySuccess(message: string) {
+    window.dispatchEvent(
+      new SnackEvent(this.EVENT_NAME, {
+        detail: { id: v4(), variant: 'success', message },
+      }),
+    );
+  }
+
+  static dispatchByWarning(message: string) {
+    window.dispatchEvent(
+      new SnackEvent(this.EVENT_NAME, {
+        detail: { id: v4(), variant: 'warning', message },
+      }),
+    );
+  }
+
+  static dispatchByError(message: string) {
+    window.dispatchEvent(
+      new SnackEvent(this.EVENT_NAME, {
+        detail: { id: v4(), variant: 'error', message },
+      }),
+    );
+  }
+
+  static dispatchByException(exception: HttpClientException) {
+    window.dispatchEvent(
+      new SnackEvent(this.EVENT_NAME, {
+        detail: { id: v4(), variant: exception.status < 500 ? 'warning' : 'error', message: exception.message },
+      }),
+    );
   }
 }

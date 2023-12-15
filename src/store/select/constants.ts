d4542b508aaa64136e6a17eb125547a3d629718b
choewy <choewy32@gmail.com>
2023-12-16 02:46:34 +0900

    feature/select store

---
+++ src/store/select/constants.ts
@@ -0,0 +1,12 @@
+import { SelectListQuery, SelectListResponse } from '@service';
+
+export const SELECT_STORE_DEFALUT_QUERY: SelectListQuery = {
+  skip: 0,
+  take: 20,
+};
+
+export const SELECT_STORE_DEFAULT_LIST: SelectListResponse = {
+  total: 0,
+  rows: [],
+  query: SELECT_STORE_DEFALUT_QUERY,
+};
---
+++ src/store/select/select.store.ts
@@ -0,0 +1,35 @@
+import { RecoilStore } from '@core';
+
+import { SelectStoreProps } from './types';
+import { SELECT_STORE_DEFALUT_QUERY, SELECT_STORE_DEFAULT_LIST } from './constants';
+
+export class SelectStore extends RecoilStore<SelectStoreProps> {
+  constructor() {
+    super(SelectStore.name, {
+      users: {
+        list: SELECT_STORE_DEFAULT_LIST,
+        query: SELECT_STORE_DEFALUT_QUERY,
+      },
+      roles: {
+        list: SELECT_STORE_DEFAULT_LIST,
+        query: SELECT_STORE_DEFALUT_QUERY,
+      },
+      category: {
+        businesses: {
+          list: SELECT_STORE_DEFAULT_LIST,
+          query: SELECT_STORE_DEFALUT_QUERY,
+        },
+        industries: {
+          list: SELECT_STORE_DEFAULT_LIST,
+          query: SELECT_STORE_DEFALUT_QUERY,
+        },
+        tasks: {
+          list: SELECT_STORE_DEFAULT_LIST,
+          query: SELECT_STORE_DEFALUT_QUERY,
+        },
+      },
+    });
+  }
+}
+
+export const selectStore = new SelectStore();

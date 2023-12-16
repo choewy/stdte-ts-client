029b91f8122152011aa2e5892d1ce165cf18c1c7
choewy <choewy32@gmail.com>
2023-12-16 14:13:32 +0900

    feature/user page table, table body

---
+++ src/page/user/components/table/user-page-table-body.tsx
@@ -0,0 +1,24 @@
+import { FunctionComponent } from 'react';
+
+import { TableBody } from '@mui/material';
+
+import { userStore } from '@store';
+import { UserPageTableBodyRow } from './user-page-table-body-row';
+
+export const UserPageTableBody: FunctionComponent = () => {
+  const { list } = userStore.useValue();
+
+  return (
+    <TableBody>
+      {list.rows.map((row, index) => (
+        <UserPageTableBodyRow
+          {...{
+            key: ['user-page-table-row', row.id, index].join('-'),
+            index,
+            row,
+          }}
+        />
+      ))}
+    </TableBody>
+  );
+};

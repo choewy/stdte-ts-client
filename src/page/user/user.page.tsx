c36c851592fa438e41d39e1bf32adb58632e9a08
choewy <choewy32@gmail.com>
2023-12-16 17:19:26 +0900

    feature/user page

--- src/page/user/user.page.tsx
+++ src/page/user/user.page.tsx
@@ -1,5 +1,36 @@
 import { FunctionComponent } from 'react';
 
+import { Box } from '@mui/material';
+
+import { userHook } from '@hook';
+
+import { UserPageUpdateDialog, UserPageToolbar, UserPageTable } from './components';
+import { credentialsStore } from '@store';
+import { roleService } from '@service';
+import { RolePolicyLevel } from '@common';
+
 export const UserPage: FunctionComponent = () => {
-  return <div>UserPage</div>;
+  const credentials = credentialsStore.useValue();
+
+  const canUpdate = roleService.can(credentials, 'user', RolePolicyLevel.Update);
+
+  userHook.useMount();
+  userHook.useUnMount();
+
+  return (
+    <Box
+      sx={{
+        mt: 2,
+        paddingX: 3,
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <UserPageUpdateDialog />
+      <UserPageToolbar />
+      <UserPageTable canUpdate={canUpdate} />
+    </Box>
+  );
 };

4e0f077c5621f02d3df7ee464f8bc0f3ac227fee
choewy <choewy32@gmail.com>
2023-12-16 15:37:51 +0900

    feature/credentials page table buttons

--- src/page/credentials/components/table/credentials-page-table-body-row-change-password-button.tsx
+++ src/page/credentials/components/table/credentials-page-table-body-row-change-password-button.tsx
@@ -1,28 +1,12 @@
 import { FunctionComponent } from 'react';
 
-import { Link } from '@mui/material';
-
-import { sizeService } from '@service';
 import { dialogHook } from '@hook';
+import { TableCellButton } from '@component';
 
 export const CredentialsPageTableBodyRowChangePasswordButton: FunctionComponent<{
   id: number;
 }> = ({ id }) => {
-  const text = '비밀번호변경';
-
   const onClick = dialogHook.useCredentialsPageUpdatePasswordDialogCallback(id, true);
 
-  return (
-    <Link
-      {...{
-        children: text,
-        onClick,
-        sx: sizeService.getWidthByTextLength(
-          text,
-          { width: true, minWidth: true },
-          { cursor: 'pointer', textWrap: 'nowrap' },
-        ),
-      }}
-    />
-  );
+  return <TableCellButton text="비밀번호변경" onClick={onClick} />;
 };

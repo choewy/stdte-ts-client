fa3c0afefc9fe8a5e753776d196cdbbce46cd974
choewy <choewy32@gmail.com>
2023-12-17 20:05:20 +0900

    feature/select customers

--- src/service/select/select-http.service.ts
+++ src/service/select/select-http.service.ts
@@ -3,35 +3,42 @@ import { HttpService } from '@core';
 import { SelectList, SelectListQuery } from './types';
 
 export class SelectHttpService extends HttpService {
-  async getUsers(query: SelectListQuery) {
+  async getUserList(query: SelectListQuery) {
     return this.get<SelectList>(this.url('users'), {
       params: query,
       delay: 250,
     });
   }
 
-  async getRoles(query: SelectListQuery) {
+  async getRoleList(query: SelectListQuery) {
     return this.get<SelectList>(this.url('roles'), {
       params: query,
       delay: 250,
     });
   }
 
-  async getBusinessCategories(query: SelectListQuery) {
+  async getCustomerList(query: SelectListQuery) {
+    return this.get<SelectList>(this.url('customers'), {
+      params: query,
+      delay: 250,
+    });
+  }
+
+  async getBusinessCategoryList(query: SelectListQuery) {
     return this.get<SelectList>(this.url('category/businesses'), {
       params: query,
       delay: 250,
     });
   }
 
-  async getIndustryCategories(query: SelectListQuery) {
+  async getIndustryCategoryList(query: SelectListQuery) {
     return this.get<SelectList>(this.url('category/industries'), {
       params: query,
       delay: 250,
     });
   }
 
-  async getTaskCategories(query: SelectListQuery) {
+  async getTaskCategoryList(query: SelectListQuery) {
     return this.get<SelectList>(this.url('category/tasks'), {
       params: query,
       delay: 250,

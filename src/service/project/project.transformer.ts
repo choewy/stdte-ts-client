d4708c0b09204d3bb01f65c0fdaa45c9a700b2c3
choewy <choewy32@gmail.com>
2023-12-19 17:18:01 +0900

    fix/project transformer

--- src/service/project/project.transformer.ts
+++ src/service/project/project.transformer.ts
@@ -5,21 +5,21 @@ export class ProjectTransformer {
     return {
       ...body,
       amount: body.amount.replaceAll(',', ''),
-      customer: body.customer === 0 ? null : 0,
-      businessCategory: body.businessCategory === 0 ? null : 0,
-      industryCategory: body.industryCategory === 0 ? null : 0,
-      taskCategory: body.taskCategory === 0 ? null : 0,
+      customer: body.customer === 0 ? null : body.customer,
+      businessCategory: body.businessCategory === 0 ? null : body.businessCategory,
+      industryCategory: body.industryCategory === 0 ? null : body.industryCategory,
+      taskCategory: body.taskCategory === 0 ? null : body.taskCategory,
     };
   }
 
-  updateRow(body: Partial<ProjectUpdateBody>) {
+  updateRow(body: ProjectUpdateBody) {
     return {
       ...body,
-      amount: (body.amount ?? '').replaceAll(',', ''),
-      customer: body.customer === 0 ? null : 0,
-      businessCategory: body.businessCategory === 0 ? null : 0,
-      industryCategory: body.industryCategory === 0 ? null : 0,
-      taskCategory: body.taskCategory === 0 ? null : 0,
+      amount: body.amount.replaceAll(',', ''),
+      customer: body.customer === 0 ? null : body.customer,
+      businessCategory: body.businessCategory === 0 ? null : body.businessCategory,
+      industryCategory: body.industryCategory === 0 ? null : body.industryCategory,
+      taskCategory: body.taskCategory === 0 ? null : body.taskCategory,
     };
   }
 }

b0819a94c1aa72e29d8c15a88e13f1c207e974d6
choewy <choewy32@gmail.com>
2023-12-20 20:49:55 +0900

    refactor/credentials table button column name

--- src/page/credentials/components/table/credentials-page-table-head.tsx
+++ src/page/credentials/components/table/credentials-page-table-head.tsx
@@ -13,7 +13,7 @@ export const CredentialsPageTableHead: FunctionComponent = () => {
         <TableValueCell value="이름" />
         <TableValueCell value="가입일시" />
         <TableValueCell value="수정일시" />
-        <TableValueCell />
+        <TableValueCell value="상태변경/비밀번호변경" />
       </TableRow>
     </TableHead>
   );

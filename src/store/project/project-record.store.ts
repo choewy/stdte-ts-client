8c1c525292f894dc8fa468b7f6769bcdaa22c793
choewy <choewy32@gmail.com>
2023-12-20 17:13:37 +0900

    feature/project record store

---
+++ src/store/project/project-record.store.ts
@@ -0,0 +1,29 @@
+import { RecoilStore } from '@core';
+
+import {
+  PROJECT_RECEORD_ORDER_LIST_QUERY,
+  PROJECT_RECEORD_SALE_LIST_QUERY,
+  PROJECT_RECORD_ORDER_LIST,
+  PROJECT_RECORD_SALE_LIST,
+} from '@service';
+
+import { ProjectRecordStoreProps } from './types';
+
+export class ProjectRecordStore extends RecoilStore<ProjectRecordStoreProps> {
+  constructor() {
+    super(ProjectRecordStore.name, {
+      order: {
+        load: true,
+        list: PROJECT_RECORD_ORDER_LIST,
+        query: PROJECT_RECEORD_ORDER_LIST_QUERY,
+      },
+      sale: {
+        load: true,
+        list: PROJECT_RECORD_SALE_LIST,
+        query: PROJECT_RECEORD_SALE_LIST_QUERY,
+      },
+    });
+  }
+}
+
+export const projectRecordStore = new ProjectRecordStore();
--- src/store/project/types.ts
+++ src/store/project/types.ts
@@ -1,7 +1,20 @@
-import { ProjectList, ProjectListQuery } from '@service';
+import { ProjectList, ProjectListQuery, ProjectRecordList, ProjectRecordListQuery } from '@service';
 
 export type ProjectStoreProps = {
   load: boolean;
   list: ProjectList;
   query: ProjectListQuery;
+};
+
+export type ProjectRecordStoreProps = {
+  order: {
+    load: boolean;
+    list: ProjectRecordList;
+    query: ProjectRecordListQuery;
+  };
+  sale: {
+    load: boolean;
+    list: ProjectRecordList;
+    query: ProjectRecordListQuery;
+  };
 };

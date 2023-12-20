6cf29132654adef0818216085c59e7224ad3c140
choewy <choewy32@gmail.com>
2023-12-20 20:45:49 +0900

    refactor/user table

--- src/page/user/components/table/user-page-table-body-row.tsx
+++ src/page/user/components/table/user-page-table-body-row.tsx
@@ -16,20 +16,28 @@ export const UserPageTableBodyRow: FunctionComponent<{ index: number; row: UserR
   return (
     <TableRow hover>
       <TableValueCell value={index + 1} />
-      <TableValueCell value={row.name} fullWidth />
-      <TableValueCell value={row.credentials.email} />
-      <TableValueCell value={enumService.userStatusToText(row.status)} />
-      <TableValueCell value={row.role?.name ?? '없음'} />
-      <TableValueCell value={DateTime.fromJSDate(new Date(row.enteringDay)).toSQLDate() ?? '-'} />
-      <TableValueCell value={DateTime.fromJSDate(new Date(row.resignationDay)).toSQLDate() ?? '-'} />
-      <TableValueCell value={DateTime.fromJSDate(new Date(row.birthday)).toSQLDate() ?? '-'} />
-      <TableValueCell value={row.phone === '' ? '-' : row.phone} />
-      <TableValueCell value={row.scienceNumber === '' ? '-' : row.scienceNumber} />
-      <TableValueCell value={enumService.degreeToText(row.degree, '-')} />
-      <TableValueCell value={row.school === '' ? '-' : row.school} />
-      <TableValueCell value={row.major === '' ? '-' : row.major} />
-      <TableValueCell value={row.carType === '' ? '-' : row.carType} />
-      <TableValueCell value={row.carNumber === '' ? '-' : row.carNumber} />
+      <>
+        <TableValueCell value={row.name} fullWidth />
+        <TableValueCell value={DateTime.fromJSDate(new Date(row.birthday)).toSQLDate() ?? '-'} />
+        <TableValueCell value={row.phone === '' ? '-' : row.phone} />
+        <TableValueCell value={row.scienceNumber === '' ? '-' : row.scienceNumber} />
+      </>
+      <>
+        <TableValueCell value={row.credentials.email} />
+        <TableValueCell value={row.role?.name ?? '없음'} />
+        <TableValueCell value={enumService.userStatusToText(row.status)} />
+        <TableValueCell value={DateTime.fromJSDate(new Date(row.enteringDay)).toSQLDate() ?? '-'} />
+        <TableValueCell value={DateTime.fromJSDate(new Date(row.resignationDay)).toSQLDate() ?? '-'} />
+      </>
+      <>
+        <TableValueCell value={enumService.degreeToText(row.degree, '-')} />
+        <TableValueCell value={row.school === '' ? '-' : row.school} />
+        <TableValueCell value={row.major === '' ? '-' : row.major} />
+      </>
+      <>
+        <TableValueCell value={row.carType === '' ? '-' : row.carType} />
+        <TableValueCell value={row.carNumber === '' ? '-' : row.carNumber} />
+      </>
       <TableValueCell value={DateTime.fromJSDate(new Date(row.createdAt)).toFormat('yyyy-MM-dd HH:mm:ss')} fixedWidth />
       <TableValueCell value={DateTime.fromJSDate(new Date(row.updatedAt)).toFormat('yyyy-MM-dd HH:mm:ss')} fixedWidth />
       {canUpdate && <TableComponentCell components={<UserPageTableBodyRowUpdateButton {...{ row }} />} />}
--- src/page/user/components/table/user-page-table-head.tsx
+++ src/page/user/components/table/user-page-table-head.tsx
@@ -8,24 +8,44 @@ export const UserPageTableHead: FunctionComponent<{ canUpdate: boolean }> = ({ c
   return (
     <TableHead>
       <TableRow>
-        <TableValueCell value="행번호" />
-        <TableValueCell value="이름" />
-        <TableValueCell value="이메일" />
-        <TableValueCell value="재직상태" />
-        <TableValueCell value="역할" />
-        <TableValueCell value="입사일자" />
-        <TableValueCell value="퇴사일자" />
-        <TableValueCell value="생년월일" />
-        <TableValueCell value="연락처" />
-        <TableValueCell value="과학기술인등록번호" />
-        <TableValueCell value="최종학력" />
-        <TableValueCell value="출신학교" />
-        <TableValueCell value="전공학과" />
-        <TableValueCell value="차종" />
-        <TableValueCell value="차량번호" />
-        <TableValueCell value="가입일시" />
-        <TableValueCell value="수정일시" />
-        {canUpdate && <TableValueCell />}
+        <TableValueCell value="행번호" rowSpan={2} stickyRow={0} />
+        <TableValueCell value="인적사항" colSpan={4} stickyRow={0} />
+        <TableValueCell value="계정정보" colSpan={5} stickyRow={0} />
+        <TableValueCell value="학력사항" colSpan={3} stickyRow={0} />
+        <TableValueCell value="차량정보" colSpan={2} stickyRow={0} />
+        <TableValueCell value="가입일시" rowSpan={2} stickyRow={0} />
+        <TableValueCell value="수정일시" rowSpan={2} stickyRow={0} />
+        {canUpdate && (
+          <TableValueCell
+            value={[canUpdate && '수정'].filter((v) => typeof v === 'string').join('/')}
+            rowSpan={2}
+            stickyRow={0}
+          />
+        )}
+      </TableRow>
+      <TableRow>
+        <>
+          <TableValueCell value="이름" stickyRow={1} />
+          <TableValueCell value="생년월일" stickyRow={1} />
+          <TableValueCell value="연락처" stickyRow={1} />
+          <TableValueCell value="과학기술인등록번호" stickyRow={1} />
+        </>
+        <>
+          <TableValueCell value="이메일" stickyRow={1} />
+          <TableValueCell value="역할" stickyRow={1} />
+          <TableValueCell value="재직상태" stickyRow={1} />
+          <TableValueCell value="입사일자" stickyRow={1} />
+          <TableValueCell value="퇴사일자" stickyRow={1} />
+        </>
+        <>
+          <TableValueCell value="최종학력" stickyRow={1} />
+          <TableValueCell value="출신학교" stickyRow={1} />
+          <TableValueCell value="전공학과" stickyRow={1} />
+        </>
+        <>
+          <TableValueCell value="차종" stickyRow={1} />
+          <TableValueCell value="차량번호" stickyRow={1} />
+        </>
       </TableRow>
     </TableHead>
   );

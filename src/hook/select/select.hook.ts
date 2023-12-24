46c5b194e5486b1d2fb6796063f9bce9b98b80a5
choewy <choewy32@gmail.com>
2023-12-24 18:27:54 +0900

    feature/project table filter

--- src/hook/select/select.hook.ts
+++ src/hook/select/select.hook.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import { HttpClientResponse } from '@core';
 import { selectStore } from '@store';
@@ -9,6 +9,7 @@ import {
   SELECT_LIST_QUERY,
   SelectException,
   SelectList,
+  SelectRow,
   SnackEvent,
   selectHttpService,
 } from '@service';
@@ -113,6 +114,27 @@ export class SelectHook {
     }, [customers.query, setSelect]);
   }
 
+  useCustomerList() {
+    const [rows, setRows] = useState<SelectRow[]>([]);
+
+    const getCustomerList = useCallback(async () => {
+      const res = await selectHttpService.getCustomerList({
+        skip: 0,
+        take: 10000,
+      });
+
+      if (res.ok) {
+        setRows(res.data.rows);
+      }
+    }, [setRows]);
+
+    useEffect(() => {
+      getCustomerList();
+    }, [getCustomerList]);
+
+    return rows;
+  }
+
   useMountCustomer() {
     const callback = this.useGetCustomerListCallback();
 
@@ -179,6 +201,47 @@ export class SelectHook {
         },
       }));
     }, [key, category[key].query, setSelect]);
+  }
+
+  useCategoryList(key: SelectCategoryKey) {
+    const [rows, setRows] = useState<SelectRow[]>([]);
+
+    const getCategoryList = useCallback(async () => {
+      let res: HttpClientResponse<SelectList> | undefined;
+
+      switch (key) {
+        case 'businesses':
+          res = await selectHttpService.getBusinessCategoryList({
+            skip: 0,
+            take: 10000,
+          });
+          break;
+
+        case 'industries':
+          res = await selectHttpService.getIndustryCategoryList({
+            skip: 0,
+            take: 10000,
+          });
+          break;
+
+        case 'tasks':
+          res = await selectHttpService.getTaskCategoryList({
+            skip: 0,
+            take: 10000,
+          });
+          break;
+      }
+
+      if (res?.ok) {
+        setRows(res.data.rows);
+      }
+    }, [key, setRows]);
+
+    useEffect(() => {
+      getCategoryList();
+    }, [getCategoryList]);
+
+    return rows;
   }
 
   useMountCategory(key: SelectCategoryKey) {
--- src/page/project/components/toolbar/project-page-toolbar.tsx
+++ src/page/project/components/toolbar/project-page-toolbar.tsx
@@ -1,12 +1,69 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useCallback } from 'react';
 
-import { Box, Button, ButtonGroup } from '@mui/material';
+import { Box, Button, ButtonGroup, MenuItem, SelectChangeEvent } from '@mui/material';
 
-import { dialogHook, projectHook } from '@hook';
+import { ProjectStatus } from '@common';
+import { SelectControl } from '@component';
+import { enumService } from '@service';
+import { projectStore } from '@store';
+import { dialogHook, projectHook, selectHook } from '@hook';
 
 export const ProjectPageToolbar: FunctionComponent<{
   canCreate: boolean;
 }> = ({ canCreate }) => {
+  const menuItemProps = enumService.projectStatusSelectMenuItemProps();
+  const industryCategories = selectHook.useCategoryList('industries');
+  const businessCategories = selectHook.useCategoryList('businesses');
+  const customers = selectHook.useCustomerList();
+  const taskCategories = selectHook.useCategoryList('tasks');
+
+  const [{ query }, setProject] = projectStore.useState();
+
+  const onChangeIndustryCategory = useCallback(
+    (e: SelectChangeEvent<unknown>) => {
+      const industryCategory = e.target.value === 'all' ? undefined : (e.target.value as number);
+
+      setProject((prev) => ({ ...prev, query: { ...prev.query, industryCategory } }));
+    },
+    [setProject],
+  );
+
+  const onChangeBusinessCategory = useCallback(
+    (e: SelectChangeEvent<unknown>) => {
+      const businessCategory = e.target.value === 'all' ? undefined : (e.target.value as number);
+
+      setProject((prev) => ({ ...prev, query: { ...prev.query, businessCategory } }));
+    },
+    [setProject],
+  );
+
+  const onChangeTaskMainCategory = useCallback(
+    (e: SelectChangeEvent<unknown>) => {
+      const taskMainCategory = e.target.value === 'all' ? undefined : (e.target.value as number);
+
+      setProject((prev) => ({ ...prev, query: { ...prev.query, taskMainCategory } }));
+    },
+    [setProject],
+  );
+
+  const onChangeCustomer = useCallback(
+    (e: SelectChangeEvent<unknown>) => {
+      const customer = e.target.value === 'all' ? undefined : (e.target.value as number);
+
+      setProject((prev) => ({ ...prev, query: { ...prev.query, customer } }));
+    },
+    [setProject],
+  );
+
+  const onChangeStatus = useCallback(
+    (e: SelectChangeEvent<unknown>) => {
+      const status = e.target.value === 'all' ? undefined : (e.target.value as ProjectStatus);
+
+      setProject((prev) => ({ ...prev, query: { ...prev.query, status } }));
+    },
+    [setProject],
+  );
+
   const onClickCreate = dialogHook.useProjectDialogCallback('create', true);
   const onClickDownload = projectHook.useDownloadCallback();
 
@@ -15,16 +72,88 @@ export const ProjectPageToolbar: FunctionComponent<{
       sx={{
         display: 'flex',
         width: '100%',
-        justifyContent: 'flex-end',
+        justifyContent: 'space-between',
         alignItems: 'flex-end',
-        height: 40,
+        height: 45,
         mb: 1,
       }}
     >
-      <ButtonGroup variant="outlined">
-        {canCreate && <Button {...{ children: '등록', size: 'small', onClick: onClickCreate }} />}
-        <Button {...{ children: '다운로드', size: 'small', onClick: onClickDownload }} />
-      </ButtonGroup>
+      <Box sx={{ display: 'flex', justifyContent: 'flex-start', margin: 0, gap: 1, flex: 2 }}>
+        <SelectControl
+          label="산업분야"
+          size="small"
+          sx={{ margin: 0, height: 40 }}
+          value={query.industryCategory ?? 'all'}
+          onChange={onChangeIndustryCategory}
+        >
+          <MenuItem value="all">전체</MenuItem>
+          {industryCategories.map((item, i) => (
+            <MenuItem key={['project-industry-query-menu-item', item.id, i].join('-')} value={item.id}>
+              {item.name}
+            </MenuItem>
+          ))}
+        </SelectControl>
+        <SelectControl
+          label="사업구분"
+          size="small"
+          sx={{ margin: 0, height: 40 }}
+          value={query.businessCategory ?? 'all'}
+          onChange={onChangeBusinessCategory}
+        >
+          <MenuItem value="all">전체</MenuItem>
+          {businessCategories.map((item, i) => (
+            <MenuItem key={['project-business-query-menu-item', item.id, i].join('-')} value={item.id}>
+              {item.name}
+            </MenuItem>
+          ))}
+        </SelectControl>
+        <SelectControl
+          label="고객사"
+          size="small"
+          sx={{ margin: 0, height: 40 }}
+          value={query.customer ?? 'all'}
+          onChange={onChangeCustomer}
+        >
+          <MenuItem value="all">전체</MenuItem>
+          {customers.map((item, i) => (
+            <MenuItem key={['project-customer-query-menu-item', item.id, i].join('-')} value={item.id}>
+              {item.name}
+            </MenuItem>
+          ))}
+        </SelectControl>
+        <SelectControl
+          label="상태"
+          size="small"
+          sx={{ margin: 0, height: 40 }}
+          value={query.status ?? 'all'}
+          onChange={onChangeStatus}
+        >
+          <MenuItem value="all">전체</MenuItem>
+          {menuItemProps.map((props, i) => (
+            <MenuItem key={['project-status-query-menu-item', props.value, i].join('-')} {...props} />
+          ))}
+        </SelectControl>
+        <SelectControl
+          label="수행업무구분"
+          size="small"
+          sx={{ margin: 0, height: 40 }}
+          value={query.taskMainCategory ?? 'all'}
+          onChange={onChangeTaskMainCategory}
+        >
+          <MenuItem value="all">전체</MenuItem>
+          {taskCategories.map((item, i) => (
+            <MenuItem key={['project-task-query-menu-item', item.id, i].join('-')} value={item.id}>
+              {item.name}
+            </MenuItem>
+          ))}
+        </SelectControl>
+      </Box>
+      <Box sx={{ display: 'flex', justifyContent: 'flex-end', flex: 1 }}>
+        <ButtonGroup variant="outlined">
+          {canCreate && <Button {...{ children: '등록', size: 'small', onClick: onClickCreate }} />}
+          <Button {...{ children: '다운로드', size: 'small', onClick: onClickDownload }} />
+        </ButtonGroup>
+      </Box>
     </Box>
   );
 };

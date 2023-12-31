87be1c43985fd3a12643672c11d35e5d9d9e6e4f
choewy <choewy32@gmail.com>
2023-12-31 20:55:11 +0900

    feature/time memo http service methods

--- src/service/time-memo/time-memo-http.service.ts
+++ src/service/time-memo/time-memo-http.service.ts
@@ -1,10 +1,18 @@
 import { HttpService } from '@core';
 
-import { TimeMemoList, TimeMemoListQuery } from './types';
+import { TimeMemoList, TimeMemoListQuery, TimeMemoUpsertBody } from './types';
 
 export class TimeMemoHttpService extends HttpService {
   async getList(query: TimeMemoListQuery) {
-    return this.post<TimeMemoList>(this.url(), query, { delay: 250 });
+    return this.post<TimeMemoList>(this.url(), query);
+  }
+
+  async upsertRow(body: TimeMemoUpsertBody) {
+    return this.patch<null>(this.url(), body);
+  }
+
+  async deleteRow(id: number) {
+    return this.delete<null>(this.url(id));
   }
 }
 
